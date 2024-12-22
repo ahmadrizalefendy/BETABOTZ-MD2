516cc74ec05f2ef2b67d151a007c03e33d0dfd7e
dana <danaputra1001@gmail.com>
2024-12-22 21:03:48 +0700

    antispam, add fitur open ai tag, getpp, coutdown

--- plugins/_openai-tag.js
+++ plugins/_openai-tag.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+let search = require("yt-search");
 
 let handler = async (m, { conn, text, command }) => {
 // kosong
@@ -55,8 +56,155 @@ handler.before = async (m, { conn }) => {
                     return true;
                 }
 
+                if (filter.toLowerCase().startsWith('/lagu')) {
+                    const songprompt = filter.replace('/lagu', '').trim();
+                    if (!songprompt) {
+                        await m.reply('Silakan berikan judul lagu yang akan di cari.');
+                        return true;
+                    }
+                        await conn.sendPresenceUpdate('composing', m.chat);
+                    
+                                const look = await search(songprompt);
+                                const convert = look.videos[0];
+                                if (!convert) throw 'Video/Audio Tidak Ditemukan';
+                                if (convert.seconds >= 3600) {
+                                    return conn.reply(m.chat, 'Video is longer than 1 hour!', m);
+                                } else {
+                                    let audioUrl;
+                                    try {
+                                        audioUrl = await youtube(convert.url);
+                                    } catch (e) {
+                                        conn.reply(m.chat, 'Please wait...', m);
+                                        audioUrl = await youtube(convert.url);
+                                    }
+                        
+                                    let caption = '';
+                                    caption += `∘ Title : ${convert.title}\n`;
+                                    caption += `∘ Ext : Search\n`;
+                                    caption += `∘ ID : ${convert.videoId}\n`;
+                                    caption += `∘ Duration : ${convert.timestamp}\n`;
+                                    caption += `∘ Viewers : ${convert.views}\n`;
+                                    caption += `∘ Upload At : ${convert.ago}\n`;
+                                    caption += `∘ Author : ${convert.author.name}\n`;
+                                    caption += `∘ Channel : ${convert.author.url}\n`;
+                                    caption += `∘ Url : ${convert.url}\n`;
+                                    caption += `∘ Description : ${convert.description}\n`;
+                                    caption += `∘ Thumbnail : ${convert.image}`;
+                        
+                                    await conn.sendMessage(m.chat, {
+                                        audio: {
+                                            url: audioUrl.result.mp3
+                                        },
+                                        mimetype: 'audio/mpeg',
+                                        contextInfo: {
+                                            externalAdReply: {
+                                                title: convert.title,
+                                                body: "",
+                                                thumbnailUrl: convert.image,
+                                                sourceUrl: audioUrl.mp3,
+                                                mediaType: 1,
+                                                showAdAttribution: true,
+                                                renderLargerThumbnail: true
+                                            }
+                                        }
+                                    }, {
+                                        quoted: m
+                                    });
+                                }
+                            
+                    return true;
+                }
+                async function youtube(url) {
+                    try {
+                    const { data } = await axios.get("https://api.betabotz.eu.org/api/download/yt?url="+url+"&apikey="+lann)
+                    return data;
+                    } catch (e) {
+                    return e;
+                    }
+                 }
+
                 await conn.sendPresenceUpdate('composing', m.chat);
-                
+
+                if (filter.toLowerCase().startsWith('/video')) {
+                    const searchvideo = filter.replace('/video', '').trim();
+                    if (!searchvideo) {
+                        await m.reply('Silakan berikan judul video yang ingin dicari.');
+                        return true;
+                    }
+
+                    try {
+                        await conn.sendPresenceUpdate('composing', m.chat);
+                         try {
+                                const look = await search(searchvideo);
+                                const convert = look.videos[0];
+                                if (!convert) throw 'Video/Audio Tidak Ditemukan';
+                                if (convert.seconds >= 3600) {
+                                    return conn.reply(m.chat, 'Video is longer than 1 hour!', m);
+                                } else {
+                                    let videoUrl;
+                                    try {
+                                        videoUrl = await yts(convert.url);
+                                    } catch (e) {
+                                        conn.reply(m.chat, 'Please wait...', m);
+                                        videoUrl = await yts(convert.url);
+                                    }
+                        
+                                    let caption = '';
+                                    caption += `∘ Title : ${convert.title}\n`;
+                                    caption += `∘ Ext : Search\n`;
+                                    caption += `∘ ID : ${convert.videoId}\n`;
+                                    caption += `∘ Duration : ${convert.timestamp}\n`;
+                                    caption += `∘ Viewers : ${convert.views}\n`;
+                                    caption += `∘ Upload At : ${convert.ago}\n`;
+                                    caption += `∘ Author : ${convert.author.name}\n`;
+                                    caption += `∘ Channel : ${convert.author.url}\n`;
+                                    caption += `∘ Url : ${convert.url}\n`;
+                                    caption += `∘ Description : ${convert.description}\n`;
+                                    caption += `∘ Thumbnail : ${convert.image}`;
+                    
+                        
+                                    await conn.sendMessage(m.chat, {
+                                        video: {
+                                            url: videoUrl.result.mp4
+                                        },
+                                        mimetype: 'video/mp4',
+                                        contextInfo: {
+                                            externalAdReply: {
+                                                title: convert.title,
+                                                body: "",
+                                                thumbnailUrl: convert.image,
+                                                sourceUrl: videoUrl.mp4,
+                                                mediaType: 1,
+                                                showAdAttribution: true,
+                                                renderLargerThumbnail: true
+                                            }
+                                        }
+                                    }, {
+                                        quoted: m
+                                    });
+                                }
+                            } catch (e) {
+                                conn.reply(m.chat, `*Error:* ` + e, m);
+                            }
+
+
+                    } catch (error) {
+                        console.error(error)
+                        await m.reply('Terjadi kesalahan saat mencari video. Mohon coba lagi.');
+                    }
+                    return true;
+                }
+                async function yts(url) {
+                   try {
+                   const { data } = await axios.get("https://api.betabotz.eu.org/api/download/ytmp4?url="+url+"&apikey="+lann)
+                   return data;
+                   } catch (e) {
+                   return e;
+                   }
+                }
+
+                await conn.sendPresenceUpdate('composing', m.chat);
+    
                 if (!filter) {
                     const empty_response = [
                         `Hi ${m.name}, how can I assist you today?`,
@@ -79,9 +227,6 @@ handler.before = async (m, { conn }) => {
                 if ([".", "#", "!", "/", "\\"].some(prefix => filter.startsWith(prefix))) return;
                 
                 const previousMessages = conn.selfai[m.sender].sessionChat || [];
-                /*
-                  Atur Prompt Disini
-                */
                 const messages = [
                     { role: "system", content: "kamu adalah BTZ, Seorang Asisten pribadi yang di buat oleh betabotz yang siap membantu kapan pun, jawab setiap pertanyaan menggunakan emoticon dan seramah mungkin kepada user jangan lupa sertakan lelcuon pada jawaban yang membuat user betah:)!" },
                     { role: "assistant", content: `Saya BTZ, asisten pribadi yang siap membantu kamu kapan pun! Apa yang bisa saya bantu hari ini?` },
